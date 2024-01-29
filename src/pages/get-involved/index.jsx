f8b03349e02fa3b16abff87d19b78fe7299c12f6
kashvi <kashvi.dwarka23@gmail.com>
2024-01-29 00:47:23 -0800

    created initial structure of feature

--- src/pages/get-involved/index.jsx
+++ src/pages/get-involved/index.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-//import styles 
+import React from "react";
+//import styles
+// import styles from "";
 
-export default function GetInvolved()
-{
-    return(
-        <>
-        Get Involved yay
-        </>
-    )
+export default function GetInvolved() {
+  return (
+    <div>
+      <h1 className={styles.sectionHeading}>Get Involved</h1>
+      <div className={styles.cardsContainer}>
+        <div className={styles.card}>
+          <p className={styles.cardText}>Physicians & Residents</p>
+          <button className={styles.detailsButton}>Details</button>
+          <button className={styles.signUpButton}>Sign Up</button>
+        </div>
+        <div className={styles.card}>
+          <p className={styles.cardText}>Undergrad. Patient Advocates</p>
+          <button className={styles.detailsButton}>Details</button>
+          <button className={styles.signUpButton}>Sign Up</button>
+        </div>
+        <div className={styles.card}>
+          <p className={styles.cardText}>Health Professional Students</p>
+          <button className={styles.detailsButton}>Details</button>
+          <button className={styles.signUpButton}>Sign Up</button>
+        </div>
+      </div>
+    </div> // must return parent element
+  );
 }
-
 GetInvolved;
