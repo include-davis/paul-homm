4488ffebf56f58bc80c55f47617f7e7d414ac245
Samantha Iraheta <sairaheta1@gmail.com>
2024-01-29 11:23:35 -0800

    added components (#2)
    * added components
    
    Added all components and their folders into the repo
    
    * workspace settings, cant ignore
    
    ---------
    
    Co-authored-by: Jay <116294517+JayJ104@users.noreply.github.com>

---
+++ src/components/committees/committeeDesription.jsx
@@ -0,0 +1,8 @@
+/* add and import styles */
+/* view and test it out by importing it to the respective page */
+export default function CommitteeDescription() {
+  return (
+    <div> committees yay</div>
+
+  );
+}
