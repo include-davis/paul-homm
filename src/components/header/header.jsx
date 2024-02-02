97c539e2f8c344390125e0c2bff00d1166ce6a82
Nandhana Selvam <nandhanaselvam@gmail.com>
2024-02-02 12:10:08 -0800

    added underline on hover. attempting bold on current page

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,29 +1,40 @@
 import styles from "@/styles/components/header/header.module.scss";
 import {useTranslations} from 'next-intl';
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 export default function Header() {
+    const router = useRouter();
     return (
         <header className={styles.header}>
-            <div title={styles.title}>
-                
+            <div className={styles.title}>
+                <div className={styles.logo}>
+                    {/*ADD LOGO HERE*/ }
+                </div>
+
+                <div className={styles.title_text}>
+                    <h1>Paul Hom Asian Clinic</h1>
+                    <h2>A Student-Run Clinic at UC Davis</h2>
+                </div>
+            
             </div>
 
+            {/*GET THIS CHECKED*/ }
             <ul className={styles.pages}>
-                <li>
+                <li className={router.asPath === '/' ? styles.currentPage : ''}>
                     <Link href="/">HOME</Link>
                 </li>
-                <li>
-                    <Link href="/">ABOUT US</Link>
+                <li className={router.asPath === '/about' ? styles.currentPage : ''}>
+                    <Link href="/about">ABOUT US</Link>
                 </li>
-                <li>
-                    <Link href="/">SERVICES</Link>
+                <li className={router.asPath === '/services' ? styles.currentPage : ''}>
+                    <Link href="/services">SERVICES</Link>
                 </li>
-                <li>
-                    <Link href="/">COMMITEES</Link>
+                <li className={router.asPath === '/committees' ? styles.currentPage : ''}>
+                    <Link href="/committees">COMMITEES</Link>
                 </li>
-                <li>
-                    <Link href="/">GET INVOLVED</Link>
+                <li className={router.asPath === '/get-involved' ? styles.currentPage : ''}>
+                    <Link href="/get-involved">GET INVOLVED</Link>
                 </li>
             </ul>
         </header>
