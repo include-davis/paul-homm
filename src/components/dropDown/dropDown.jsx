302bc6650a04d94e97a87a4f8475af925c6d1811
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-02-03 12:56:38 -0800

    Added icons and dividers to dropdown

--- src/components/dropDown/dropDown.jsx
+++ src/components/dropDown/dropDown.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 import styles from "@/styles/components/dropDown/dropDown.module.scss";
 import { LanguageContext } from "../../pages/index";
-
+import { RxDividerHorizontal } from "react-icons/rx";
 
 
 function DropDownTable({ tableState, clickLanguageFunct }) {
@@ -9,11 +9,17 @@ function DropDownTable({ tableState, clickLanguageFunct }) {
     let table;
     if (tableState==true) {
         table = <ul className={styles.table}>
+            <RxDividerHorizontal className={styles.line1 + ' ' + styles.line} />
             <div className={styles.enOption}><button onClick={() => clickLanguageFunct("English")} className={styles.options}>{"English"}</button></div>
+            <RxDividerHorizontal className={styles.line2 + ' ' + styles.line} />
             <div className={styles.cnOption}><button onClick={() => clickLanguageFunct("Chinese")} className={styles.options}>{"Chinese"}</button></div>
+            <RxDividerHorizontal className={styles.line3 + ' ' + styles.line} />
             <div className={styles.koOption}><button onClick={() => clickLanguageFunct("Korean")} className={styles.options}>{"Korean"}</button></div>
+            <RxDividerHorizontal className={styles.line4 + ' ' + styles.line} />
             <div className={styles.vnOption}><button onClick={() => clickLanguageFunct("Viet")} className={styles.options}>{"Viet"}</button></div>
+            <RxDividerHorizontal className={styles.line5 + ' ' + styles.line} />
             <div className={styles.hmOption}><button onClick={() => clickLanguageFunct("Hmong")} className={styles.options}>{"Hmong"}</button></div>
+            <RxDividerHorizontal className={styles.line6 + ' ' + styles.line} />
             <div className={styles.spOption}><button onClick={() => clickLanguageFunct("Spanish")} className={styles.options}>{"Spanish"}</button></div>
             </ul>
     }
@@ -37,28 +43,20 @@ export default function LangDropDown() {
     }
 
     function toggleDropDown() {
-        if (dropDown==false) {
-            setDropDown(prevState => !prevState)
-        }
-        else {
-            setTimeout(() => {
-                setDropDown(prevState => !prevState)
-            }, 400)
-        }
+        setDropDown(prevState => !prevState)
     }
 
     
     function clickLanguage(lang_code) {
-
         setLanguage(lang_code)
         setDropDown(prevState => !prevState)
-
     }
 
   return (
     <div className={styles.container}>
         <div className={styles.header}>
             <button>{ language }</button>
+            <img  src='/translationIcon.svg' className={styles.translationIcon} alt="Translation Symbol"/>
             {arrow}
         </div>
         <DropDownTable tableState={ dropDown } clickLanguageFunct={ clickLanguage } />
