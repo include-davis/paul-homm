0649dacec00d8606cb8386fd5055149f4e7c96dd
Nandhana Selvam <nandhanaselvam@gmail.com>
2024-02-02 23:23:46 -0800

    attempting useState for highlight on active

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,47 +1,51 @@
 import styles from "@/styles/components/header/header.module.scss";
-import {useTranslations} from 'next-intl';
+import { useTranslations } from 'next-intl';
 import Link from "next/link";
-import { useRouter } from 'next/router';
+import { useState } from "react";
 import Image from 'next/image'
 
 export default function Header() {
-    const router = useRouter();
+    const [activeLinks, setActiveLinks] = useState([false, false, false, false, false]);
+
+    const toggleActive = (index) => {
+        setActiveLinks(activeLinks.map((value, i) => i === index));
+    }
+
+    const links = [
+        { href: '/', text: 'HOME' },
+        { href: '/about', text: 'ABOUT US' },
+        { href: '/services', text: 'SERVICES' },
+        { href: '/committees', text: 'COMMITEES' },
+        { href: '/get-involved', text: 'GET INVOLVED' }
+    ];
+
     return (
         <header className={styles.header}>
             <div className={styles.title}>
-                
-                <Image 
-                    src = "/paul_hom_logo.png"
-                    width = {110}
-                    height = {110}
-                    alt = "Paul Hom logo"
+                <Image
+                    src="/paul_hom_logo.png"
+                    width={110}
+                    height={110}
+                    alt="Paul Hom logo"
                 />
-
                 <div className={styles.title_text}>
                     <h1>Paul Hom Asian Clinic</h1>
                     <h2>A Student-Run Clinic at UC Davis</h2>
                 </div>
-            
             </div>
-
-            {/*GET THIS CHECKED*/ }
-            <ul className={styles.pages}>
-                <li className={router.asPath === '/' ? styles.currentPage : ''}>
-                    <Link href="/">HOME</Link>
-                </li>
-                <li className={router.asPath === '/about' ? styles.currentPage : ''}>
-                    <Link href="/about">ABOUT US</Link>
-                </li>
-                <li className={router.asPath === '/services' ? styles.currentPage : ''}>
-                    <Link href="/services">SERVICES</Link>
-                </li>
-                <li className={router.asPath === '/committees' ? styles.currentPage : ''}>
-                    <Link href="/committees">COMMITEES</Link>
-                </li>
-                <li className={router.asPath === '/get-involved' ? styles.currentPage : ''}>
-                    <Link href="/get-involved">GET INVOLVED</Link>
-                </li>
-            </ul>
+            <div className={styles.pages}>
+                {links.map((link, index) => (
+                    <li key = {index} className = {`${styles.page_link} ${activeLinks? styles.active : ''}`}>
+                        <Link 
+                            href={`/${link.href}`} 
+                            onClick={() => toggleActive(index)}
+                        >
+                            {link.text}
+                        </Link>
+                     </li>
+                )
+            )}
+            </div>
         </header>
     );
 }
