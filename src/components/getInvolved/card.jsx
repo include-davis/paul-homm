dfa89bf18d9f2a15970305acc79b82917adcb6bd
kashvi <kashvi.dwarka23@gmail.com>
2024-02-02 19:16:49 -0800

    images are the correct size in full screen now - so many bugs with images!!

--- src/components/getInvolved/card.jsx
+++ src/components/getInvolved/card.jsx
@@ -10,38 +10,24 @@ export default function Card() {
         <h1 className={styles.sectionHeading}>Get Involved</h1>
         <div className={styles.cardsContainer}>
           <div className={styles.card}>
-            <Image
-              src="/physiciansAndResidents.png"
-              width={150}
-              height={250}
-              objectFit="contain"
-              alt="Girl Getting Her Blood Drawn"
-            />
+            <div className={styles.cardImg1}>
+              <Image />
+            </div>
             <p className={styles.cardText}>Physicians & Residents</p>
             <button className={styles.detailsButton}>Details</button>
             <button className={styles.signUpButton}>Sign Up</button>
           </div>
           <div className={styles.card}>
-            <Image
-              src="/undergrad.png"
-              width={150}
-              height={400}
-              objectFit="cover"
-              alt="Group Of Residents"
-            />
+            <div className={styles.cardImg2}>
+              <Image />
+            </div>
             <p className={styles.cardText}>Undergrad. Patient Advocates</p>
             <button className={styles.detailsButton}>Details</button>
             <button className={styles.signUpButton}>Sign Up</button>
           </div>
           <div className={styles.card}>
-            <div className={styles.cardImg}>
-              <Image
-                src="/specialtyClinics.png"
-                width={300}
-                height={400}
-                objectFit="cover"
-                alt="Group of Residents"
-              />
+            <div className={styles.cardImg3}>
+              <Image />
             </div>
 
             <p className={styles.cardText}>Health Professional Students</p>
