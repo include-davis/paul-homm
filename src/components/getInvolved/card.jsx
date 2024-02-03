61c61e373b0e63a845c9c13bc24bec438b9270c2
kashvi <kashvi.dwarka23@gmail.com>
2024-02-02 18:20:04 -0800

    issues with importing issues - pushing so Jay can take a look at it

--- src/components/getInvolved/card.jsx
+++ src/components/getInvolved/card.jsx
@@ -2,7 +2,7 @@
 /* view and test it out by importing it to the respective page */
 import styles from "@/styles/pages/get-involved/cards.module.scss";
 import Image from "next/image";
-// import AbImage from "/images/image.png";
+import AbImage from "/next.svg";
 export default function Card() {
   return (
     <div>
@@ -10,11 +10,26 @@ export default function Card() {
         <h1 className={styles.sectionHeading}>Get Involved</h1>
         <div className={styles.cardsContainer}>
           <div className={styles.card}>
+            <Image
+              src="/public/physiciansAndResidents.png"
+              width={300}
+              height={400}
+              objectFit="cover"
+              alt="Physicians And Residents"
+            />
+            <AbImage></AbImage>
             <p className={styles.cardText}>Physicians & Residents</p>
             <button className={styles.detailsButton}>Details</button>
             <button className={styles.signUpButton}>Sign Up</button>
           </div>
           <div className={styles.card}>
+            <Image
+              src="/public/undergrad.png"
+              width={300}
+              height={400}
+              objectFit="cover"
+              alt="Undergrad"
+            />
             <p className={styles.cardText}>Undergrad. Patient Advocates</p>
             <button className={styles.detailsButton}>Details</button>
             <button className={styles.signUpButton}>Sign Up</button>
