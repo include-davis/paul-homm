3d1cc30c60d81d97432eba0f974f7ef213d4e4a1
JayJ104 <123anjalijain@gmail.com>
2024-02-03 22:42:21 -0800

    committees dynamic routing simplified

---
+++ src/pages/committees/[committee].jsx
@@ -0,0 +1,37 @@
+import CommitteeDescription from "@/components/committees/committeeDesription";
+
+const data = {
+    cardiac: {
+        title: "Cardiac Committee",
+        desc: "heart stuff"
+    },
+    neuro: {
+        title: "Neuro Committee",
+        desc: "brain stuff"
+    }
+}
+
+export async function getStaticPaths(){
+    const committees = Object.keys(data);
+    const paths = committees.map((committee) => ({
+        params: { committee }
+    }));
+    return { paths, fallback: false};
+};
+
+export async function getStaticProps({ params }) {
+    const committeeData = data[params.committee];
+    return {
+      props: {
+        committeeData,
+      },
+    };
+}
+
+export default function CommitteePage({ committeeData }){
+    return(
+        <div>
+            <CommitteeDescription props={committeeData}/>
+        </div>
+    )
+};
