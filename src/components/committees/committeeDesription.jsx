fdf2a141096c06a59c9adbbc206ebf0a8932547a
Elysia Oh <esoh@ucdavis.edu>
2024-02-05 23:03:01 -0800

    General Styling to Committee descriptions page
    - Still need to implement back button

--- src/components/committees/committeeDesription.jsx
+++ src/components/committees/committeeDesription.jsx
@@ -1,11 +1,16 @@
+import styles from "@/styles/components/committeeDescription/committeeDescription.module.scss"
+
 /* add and import styles */
 /* view and test it out by importing it to the respective page */
 export default function CommitteeDescription({ props }) {
   const { title, desc } = props;
   return (
-    <div>
-      <h1>{title}</h1>
-      <h2>{desc}</h2>
+    <div className = {styles.wrapper}>
+      <button className = {styles.button}>Click</button>
+      <div className = {styles.content}>
+        <h1>{title}</h1>
+        <h2>{desc}</h2>
+      </div>
     </div>
   );
 }
