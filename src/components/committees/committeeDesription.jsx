6b3959d4455dc8df980f80661655c16a7fc3f98e
Elysia Oh <esoh@ucdavis.edu>
2024-02-07 21:20:21 -0800

    Added button and changed styling

--- src/components/committees/committeeDesription.jsx
+++ src/components/committees/committeeDesription.jsx
@@ -1,12 +1,16 @@
 import styles from "@/styles/components/committeeDescription/committeeDescription.module.scss"
-
+import { IoArrowBack } from "react-icons/io5";
 /* add and import styles */
 /* view and test it out by importing it to the respective page */
 export default function CommitteeDescription({ props }) {
   const { title, desc } = props;
   return (
     <div className = {styles.wrapper}>
-      <button className = {styles.button}>Click</button>
+      <div className = {styles.buttonClass}>
+        <a href="@/pages/committees/index.jsx" className = {styles.linkStyle}>
+          <button className = {styles.button}><IoArrowBack /><p>Back to committees</p></button> 
+          </a>
+      </div>
       <div className = {styles.content}>
         <h1>{title}</h1>
         <h2>{desc}</h2>
