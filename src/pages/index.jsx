7754871deaa3ed4039123638483992472bd35ef2
C3viche <crisemble@gmail.com>
2024-02-09 20:01:11 -0800

    basic styling with positioning and colors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,10 @@ import { Inter } from "next/font/google";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { MdPhone, MdLocationOn } from "react-icons/md";
-import { FiMessageSquare } from "react-icons/fi";
+import { BiSolidMessage } from "react-icons/bi";
 
+
+import styles from "@/styles/pages/home/home.module.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,13 +22,15 @@ export default function Home() {
   return (
     <div>
       {/* Red "Our Mission" Section */}
-      <div>
-        <h2>Our Misson</h2>
-        <p>
-            Paul Hom Asian Clinic is a 501C(3) non-profit organization committed to serving underserved and uninsured Asian populations in a linguistically and culturally sensitive manner.<br/>
-            At Paul Hom we provide free primary care to the medically underserved, quality community medicine for our local community and interpretation services for the underserved Asian community.<br/>
-            Our success is a result of the collaborative effort of all our health professional and undergraduate staff working to provide such important services.
-        </p>
+      <div className={styles.mission}>
+        <div>
+            <h2>Our Mission</h2>
+            <p>
+                Paul Hom Asian Clinic is a 501C(3) non-profit organization committed to serving underserved and uninsured Asian populations in a linguistically and culturally sensitive manner.<br/>
+                At Paul Hom we provide free primary care to the medically underserved, quality community medicine for our local community and interpretation services for the underserved Asian community.<br/>
+                Our success is a result of the collaborative effort of all our health professional and undergraduate staff working to provide such important services.
+            </p>
+        </div>
         <Image
             src="/happy-doctors.png"
             width={593}
@@ -37,53 +41,67 @@ export default function Home() {
       </div>
 
       {/* White Section with Info cards */}
-      <div>
+      <div className={styles.info}>
         {/* Visit us card with contact Info */}
-        <div>
+        <div className={styles.visit_card}>
             <h2>Visit Us!</h2>
             <p>Leave us a voicemail with your name and phone number, and we'll call you back to book an appointment.</p>
-            <MdPhone/>
-            <p>(916) 736-3966</p>
-            <FiMessageSquare/>
-            <p>Message</p>
-            <MdLocationOn/>
-            <p>6341 Folsom Blvd Sacramento, CA 95819</p>
+            <div className={styles.visit}>
+                <div className={styles.contact}>
+                    <div className={styles.row_icons}>
+                        <div className={styles.circle_frame}><MdPhone/></div>
+                        <p>(916) 736-3966</p>
+                    </div>
+                    <div className={styles.row_icons}>
+                        <div className={styles.circle_frame}><BiSolidMessage/></div>
+                        <p>Message</p>
+                    </div>
+                    
+                </div>
+                <div className={styles.circle_frame}><MdLocationOn/></div>
+                <p>6341 Folsom Blvd Sacramento, CA 95819</p>
+            </div>
+            
         </div>
-        {/* Important Closure Dates */}
-        <div>
-            <h2>Closure Dates</h2>
-            <ul>
-                <li>Feb. 10, 2024</li>
-                <li>Mar. 30, 2024</li>
-                <li>May 25, 2024</li>
-                <li>Jul. 6, 2024</li>
-                <li>Aug 31, 2024</li>
-            </ul>
-        </div>
-        {/* Upcoming Events Section */}
-        <div>
-            <h2>Upcoming Events</h2>
-            <ul>
-                <li>Jan. 7, 2023</li>
-                <li>Jan. 8, 2023</li>
-                <li>Jan. 14, 2023</li>
-                <li>Jan. 15, 2023</li>
-                <li>Jan. 29, 2023</li>
-            </ul>
-            <ul>
-                <li>Musculoskeletal Clinic</li>
-                <li>VN Cares Cancer Screening</li>
-                <li>Psychiatry Clinic</li>
-                <li>Hepatitis Vaccination</li>
-                <li>Dermatology Clinic</li>
-            </ul>
-            <Image
-                src="/events-img.png"
-                width={411}
-                height={263}
-                alt={"People holding posters"}
-                // style={styles.picture}
-            />
+        <div className={styles.dates}>
+            {/* Important Closure Dates */}
+            <div className={styles.closure_card}>
+                <h2>Closure Dates</h2>
+                <ul>
+                    <li>Feb. 10, 2024</li>
+                    <li>Mar. 30, 2024</li>
+                    <li>May 25, 2024</li>
+                    <li>Jul. 6, 2024</li>
+                    <li>Aug 31, 2024</li>
+                </ul>
+            </div>
+            {/* Upcoming Events Section */}
+            <div className={styles.events_card}>
+                <h2>Upcoming Events</h2>
+                <div className={styles.events}>
+                    <ul>
+                        <li>Jan. 7, 2023</li>
+                        <li>Jan. 8, 2023</li>
+                        <li>Jan. 14, 2023</li>
+                        <li>Jan. 15, 2023</li>
+                        <li>Jan. 29, 2023</li>
+                    </ul>
+                    <ul>
+                        <li>Musculoskeletal Clinic</li>
+                        <li>VN Cares Cancer Screening</li>
+                        <li>Psychiatry Clinic</li>
+                        <li>Hepatitis Vaccination</li>
+                        <li>Dermatology Clinic</li>
+                    </ul>
+                    <Image
+                        src="/events-img.png"
+                        width={411}
+                        height={263}
+                        alt={"People holding posters"}
+                        // style={styles.picture}
+                    />
+                </div>
+            </div>
         </div>
       </div>
     </div>
