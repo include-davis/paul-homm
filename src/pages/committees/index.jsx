5e03397f906be583360a22e04d6f2b28be68efd4
Nandhana Selvam <nandhanaselvam@gmail.com>
2024-02-09 23:41:08 -0800

    trying to make card component and do mapping

--- src/pages/committees/index.jsx
+++ src/pages/committees/index.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
-//import styles 
+import styles from "@/styles/pages/committees/committees.module.scss"
 
-const data = [{name: "Cardiopulmonary", image:}]
+//actual component?
+function CommitteeCard(props) {
+    return (
+        <div className={styles.committee_card}>
+            <Img
+                src={props.image}
+                alt={props.name}
+            />
+            <h3>{props.name}</h3>
+        </div>
+    )
+}
 
-export default function committees({props}) {
+
+
+//data to make cards
+const data = [
+    { name: "Cardiopulmonary", image:"/images/committees/cardiopulmonary.png" }, 
+    { name: "Covered California", image:"/images/committees/covered_california.png"}, 
+    { name: "Dermatology", image:"/images/committees/dermatology.png"},
+
+    { name: "Diabetes", image:"/images/committees/diabetes.png"},
+    { name: "Patient Assistance Program", image:"/images/committees/patient_assistance_program.png"},
+    { name: "Hepatitis", image:"/images/committees/hepatitis.png"},
+
+    { name: "Neurology", image:"/images/committees/neurology.png"},
+    { name: "Women's Health", image:"/images/committees/womens_health.png"},
+    { name: "Ophthalmology", image:"/images/committees/ophthalmology.png"}
+]
+
+//mapping
+export default function committees() {
     return(
-        <>
-        committees yay
-        </>
+        <div className={styles.committees_page}>
+            <div className={styles.committees_cards}>
+                {data.map((committee, index) => (
+                    <CommitteeCard 
+                        key = {index}
+                        name = {committee.name}
+                        image = {committee.image}
+                    />
+                ))}
+            </div>
+        </div>
     )
 }
 
