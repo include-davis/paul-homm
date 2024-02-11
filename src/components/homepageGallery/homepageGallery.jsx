e03c8327ca17b89604d53fb6eb3223f89d01e0da
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-02-10 17:56:16 -0800

    Added a couple elements necessary to the homepage gallery

--- src/components/homepageGallery/homepageGallery.jsx
+++ src/components/homepageGallery/homepageGallery.jsx
@@ -1,6 +1,18 @@
+import React, { useState } from 'react';
 import styles from "@/styles/components/homepageGallery/homepageGallery.module.scss";
 
-export default function HomepageGallery() { //animation will be done in css, but this component rerenders if specific image is clicked and it restarts the animation from that image
+
+
+export default function HomepageGallery() { 
+  const [activeImg, setActiveImg] = useState(0);
+  let images = [<img  src='/homepageGallery/HomepageGalleryImage1.jpg' alt="Divider"/>, <img  src='/homepageGallery/HomepageGalleryImage2.png' alt="Divider"/>, <img  src='/homepageGallery/HomepageGalleryImage3.png' alt="Divider"/>, <img  src='/homepageGallery/HomepageGalleryImage4.png' alt="Divider"/>]
+  
+  useEffect(() => {
+    const autoPlayInterval = setInterval(nextSlide, interval);
+    return () => {
+      clearInterval(autoPlayInterval);
+    };
+  }, [3000]);
   return (
     <div className={styles.container}>
       <h1>Howdy</h1>
