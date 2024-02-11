47c2a0d404d576c65d050d2a21f90129302d2c9d
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-02-11 01:44:31 -0800

    Implemented slideshow autoplay

--- src/components/homepageGallery/homepageGallery.jsx
+++ src/components/homepageGallery/homepageGallery.jsx
@@ -1,20 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "@/styles/components/homepageGallery/homepageGallery.module.scss";
 
 
 
 export default function HomepageGallery() { 
   const [activeImg, setActiveImg] = useState(0);
-  let images = [<img  src='/homepageGallery/HomepageGalleryImage1.jpg' alt="Divider"/>, <img  src='/homepageGallery/HomepageGalleryImage2.png' alt="Divider"/>, <img  src='/homepageGallery/HomepageGalleryImage3.png' alt="Divider"/>, <img  src='/homepageGallery/HomepageGalleryImage4.png' alt="Divider"/>]
+  let curr = [<img  src='/homepageGallery/HomepageGalleryImage1.jpg' alt="Divider" className={styles.frame+' '+styles.curr}/>, <img  src='/homepageGallery/HomepageGalleryImage2.png' alt="Divider" className={styles.frame+' '+styles.curr}/>, <img  src='/homepageGallery/HomepageGalleryImage3.png' alt="Divider" className={styles.frame+' '+styles.curr}/>, <img  src='/homepageGallery/HomepageGalleryImage4.png' alt="Divider" className={styles.frame+' '+styles.curr}/>]
+  let next = [<img  src='/homepageGallery/HomepageGalleryImage1.jpg' alt="Divider" className={styles.frame+' '+styles.next}/>, <img  src='/homepageGallery/HomepageGalleryImage2.png' alt="Divider" className={styles.frame+' '+styles.next}/>, <img  src='/homepageGallery/HomepageGalleryImage3.png' alt="Divider" className={styles.frame+' '+styles.next}/>, <img  src='/homepageGallery/HomepageGalleryImage4.png' alt="Divider" className={styles.frame+' '+styles.next}/>]
   
+
+  function go_nextFrame() {
+    if (activeImg!=3) {
+      setActiveImg(activeImg+1);
+      }
+    else {
+      setActiveImg(0);
+      }
+  }
+
   useEffect(() => {
-    const autoPlayInterval = setInterval(nextSlide, interval);
+    const autoPlay = setInterval(() => { go_nextFrame(); }, 4000);
     return () => {
-      clearInterval(autoPlayInterval);
+      clearInterval(autoPlay);
     };
-  }, [3000]);
+  }, [activeImg]);
+
+  let currFrame = curr[activeImg];
+  let nextFrame;
+  if (activeImg!=3) {
+    nextFrame = next[activeImg+1];
+    }
+  else {
+    nextFrame = next[0];
+    }
+  
   return (
     <div className={styles.container}>
+      <div key={activeImg} className={styles.frameContainer}>
+        {currFrame}
+        {nextFrame}
+      </div>
       <h1>Howdy</h1>
     </div>
   );
