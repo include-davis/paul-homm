b5c635c1abdd84a14c6318e955df311df241ef4b
kashvi <kashvi.dwarka23@gmail.com>
2024-02-16 18:39:12 -0800

    deleted content from old branch (accidentally branched off assignment 1)

--- src/components/getInvolved/card.jsx
+++ src/components/getInvolved/card.jsx
@@ -4,40 +4,5 @@ import styles from "@/styles/pages/get-involved/cards.module.scss";
 import Image from "next/image";
 
 export default function Card() {
-  return (
-    <div>
-      <div className={styles.sectionContainer}>
-        <h1 className={styles.sectionHeading}>Get Involved</h1>
-        <div className={styles.cardsContainer}>
-          <div className={styles.card}>
-            <div className={styles.cardImg1}>
-              <Image />
-              <p className={styles.cardText}>Physicians & Residents</p>
-            </div>
-
-            <button className={styles.detailsButton}>Details</button>
-            <button className={styles.signUpButton}>Sign Up</button>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.cardImg2}>
-              <Image />
-              <p className={styles.cardText}>Undergrad. Patient Advocates</p>
-            </div>
-
-            <button className={styles.detailsButton}>Details</button>
-            <button className={styles.signUpButton}>Sign Up</button>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.cardImg3}>
-              <Image />
-              <p className={styles.cardText}>Health Professional Students</p>
-            </div>
-
-            <button className={styles.detailsButton}>Details</button>
-            <button className={styles.signUpButton}>Sign Up</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div> card yay</div>;
 }
