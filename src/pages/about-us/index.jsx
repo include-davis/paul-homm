1ca9427d53593d65b5b62926656104c0512d2fee
annapandey <149274878+annapandey@users.noreply.github.com>
2024-02-16 19:56:12 -0800

    Update index.jsx

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -3,7 +3,7 @@ import flippingCard from '@/components/about-us/flippingCard';
 export default function About() {
     return (
         <>
-            <flippingCard>Card1</flippingCard>
+            <flippingCard>Card1,Card2,Card3,Card4,Card5</flippingCard>
         </>
     )
 }
