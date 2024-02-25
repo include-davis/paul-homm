13dddd3661a0004de60a9cea9c0b6292c37614d4
Elysia Oh <102720577+elysiaoh@users.noreply.github.com>
2024-02-25 12:28:28 -0800

    Updates to description page

--- src/components/committees/committeeDesription.jsx
+++ src/components/committees/committeeDesription.jsx
@@ -1,13 +1,25 @@
-import styles from "@/styles/components/committeeDescription/committeeDescription.module.scss"
+import styles from "@/styles/components/committees/committeeDescription.module.scss"
 import { IoArrowBack } from "react-icons/io5";
+import React from "react";
+import { useTranslations } from "next-intl";
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      messages: (await import(`@/messages/${locale}.json`)).default
+    }
+  };
+}
+
 /* add and import styles */
 /* view and test it out by importing it to the respective page */
 export default function CommitteeDescription({ props }) {
+  const t = useTranslations(`Example.propsComponent.${props}`);
   const { title, desc } = props;
   return (
     <div className = {styles.wrapper}>
       <div className = {styles.buttonClass}>
-        <a href="@/pages/committees/index.jsx" className = {styles.linkStyle}>
+        <a href="/committees" className = {styles.linkStyle}>
           <button className = {styles.button}><IoArrowBack /><p>Back to committees</p></button> 
           </a>
       </div>
@@ -17,4 +29,4 @@ export default function CommitteeDescription({ props }) {
       </div>
     </div>
   );
-}
+} 
