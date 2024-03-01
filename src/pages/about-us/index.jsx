2e1abcde8bbcadabb112f6618ea7d395c615dabd
Elysia Oh <102720577+elysiaoh@users.noreply.github.com>
2024-02-29 23:53:41 -0800

    Created initial carousel component + Youtube Embed
    Need the photos for additional styling

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -2,13 +2,16 @@ import React,{ useState, useEffect, useRef } from 'react';
 import YoutubeEmbed from "@/components/about-us/YoutubeEmbed";
 //import YouTube from "react-youtube";
 import styles from "@/styles/pages/about/about.module.scss";
+import Carousel from "@/components/about-us/carousel";
 //import styles 
 export default function About()
 {
+    const carouselData = ['frame1', 'frame2', 'frame3', /* Add more frames as needed */];
     return(
         <div className={styles.wrapperClass}>
             <h1>The Legacy of Paul Hom</h1>
             <h2>We are the oldest Asian clinic in the United States.</h2> 
+            <Carousel data={carouselData} />
             <p>Dr. Paul Hom had an untiring commitment to the clinic and to its purpose in serving patients of all ages in the Asian community who lack adequate healthcare due to cultural, linguistic, or economic barriers. Healthcare continues to be provided at no charge to all patients. </p>
             <YoutubeEmbed embedId="UUguG3tATJE" />
             <div className={styles.listClass}>
