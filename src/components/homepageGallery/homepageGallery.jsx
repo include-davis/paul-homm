8e70bdbecfe1c2d61b16f1363a9aeb4663363aa5
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-03-01 21:24:37 -0800

    Removed nav button clicking + Added back/left arrow animation

--- src/components/homepageGallery/homepageGallery.jsx
+++ src/components/homepageGallery/homepageGallery.jsx
@@ -7,17 +7,21 @@ import { FaArrowCircleRight } from "react-icons/fa";
 
 
 export default function HomepageGallery() { 
-  const [activeImg, setActiveImg] = useState(0);
+  const [activeImg, setActiveImg] = useState(0); // activeImg can hold values 0-7 indicating which image from the lists below to use at each render
   let imgPaths = ['/homepageGallery/HomepageGalleryImage1.jpg', '/homepageGallery/HomepageGalleryImage2.png', '/homepageGallery/HomepageGalleryImage3.png', '/homepageGallery/HomepageGalleryImage4.png']
+  // for each image, hold a version of the image with different css stylings corresponding to possible roles it can take on each rerender, whether it is the previously shown image in the gallery or the currently shown image in the gallery 
   let prev = []
   let curr = []
-  let select = []
+  let prevR = [] // images in these 2 "R" lists will have stylings necessary to animate the backwards/Reverse sliding (necessary for when the left arrow in the mobile ver. is tapped)
+  let currR = []
   for (const element of imgPaths) {
     prev.push(<img  src={element} alt="Previous Gallery Image" className={styles.frame+' '+styles.prev}/>)
     curr.push(<img  src={element} alt="Current Gallery Image" className={styles.frame+' '+styles.curr}/>)
-    select.push(<img  src={element} alt="Current Gallery Image" className={styles.selectedFrame}/>)
+    prevR.push(<img  src={element} alt="Previous Gallery Image" className={styles.frameR+' '+styles.prevR}/>)
+    currR.push(<img  src={element} alt="Current Gallery Image" className={styles.frameR+' '+styles.curr}/>)
   }
 
+  
   function go_nextFrame() {
     if (activeImg%4!=3) {
       setActiveImg(activeImg%4+1);
@@ -27,16 +31,12 @@ export default function HomepageGallery() {
       }
   }
 
-  function select_Frame(index) {
-    setActiveImg(index);
-  }
-
   function select_Prev() {
-    if (activeImg!=0) {
-      setActiveImg(activeImg-1);
+    if (activeImg%4!=0) {
+      setActiveImg(activeImg-1+8);
       }
     else {
-      setActiveImg(3);
+      setActiveImg(3+8);
       }
   }
 
@@ -50,32 +50,40 @@ export default function HomepageGallery() {
 
   let currFrame;
   let prevFrame;
-  let selectedFrame;
+
   if (activeImg>=4) {
-    selectedFrame = select[activeImg%4];
+    currFrame = currR[activeImg%4];
+    if (activeImg%4==3) {
+      prevFrame = prevR[0];
+    }
+    else {
+      prevFrame = prevR[activeImg%4+1];
+    }
   }
   else if (activeImg!=0) {
-    currFrame = curr[activeImg%4];
+    currFrame = curr[activeImg];
     prevFrame = prev[activeImg-1];
     }
   else {
-    currFrame = curr[activeImg%4];
+    currFrame = curr[activeImg];
     prevFrame = prev[3];
     }
   
   let navDots = []
-  for (let i = 4; i < 8; i++) {
+  for (let i = 0; i < 4; i++) {
     if (i%4==activeImg%4) {
-      navDots.push(<button className={styles.navBttn} onClick={()=>{select_Frame(i)}}><FaCircle className={styles.selected+' '+styles.circle}/></button>)
+      navDots.push(<FaCircle className={styles.selected+' '+styles.circle}/>)
       }
     else {
-      navDots.push(<button className={styles.navBttn} onClick={()=>{select_Frame(i)}}><FaCircle className={styles.circle}/></button>)
+      navDots.push(<FaCircle className={styles.circle}/>)
       }
   }
 
   return (
     <div className={styles.gallery}>
+
       <div className={styles.container}>
+
         <div className={styles.textContainer}><div className={styles.blur}>
           <h2 div className={styles.title}>A Passion to Serve</h2>
           <p div className={styles.description}>Quality clinical services for our local community</p></div>
@@ -84,16 +92,18 @@ export default function HomepageGallery() {
           <div className={styles.left_blur}><FaArrowCircleLeft className={styles.arrow} onClick={()=>{select_Prev()}} alt="Left Arrow"/></div>
           {prevFrame}
           {currFrame}
-          {selectedFrame}
           <div className={styles.right_blur}><FaArrowCircleRight className={styles.arrow} onClick={()=>{go_nextFrame()}} alt="Right Arrow"/></div>
         </div>
+
       </div>
+
       <div className={styles.nav}>
         {navDots[0]}
         {navDots[1]}
         {navDots[2]}
         {navDots[3]}
       </div>
+    
     </div>
   );
 }
