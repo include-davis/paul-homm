ff9f05780b9f07d9bfe1f848e286115dc347bc44
Samantha Iraheta <sairaheta1@gmail.com>
2024-03-03 18:36:57 -0800

    small edit to render

--- src/components/about-us/flippingCard.jsx
+++ src/components/about-us/flippingCard.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { useTranslations } from 'next-intl';
 import styles from "@/styles/components/about-us/flippingCard.module.scss";
 
-export default function FlippingCard ({ title, content, image, alt }) {
+export default function FlippingCard({ title, content, image, alt }) {
     return (
-        <div className={styles.container}>
+
+        <div className={styles.FlipCard}>
             <h1>{title}</h1>
-            <div className={styles.FlipCard}>
-                <div className={styles.CardInner}></div>
+            <div className={styles.CardInner}>
                 <div className={styles.CardFront}>
                     <img src={image} alt={alt} />
                 </div>
-                <div className={styles.FlipBack}>
-                    <p>{content}</p>
-                </div>
+
+            </div>
+            <div className={styles.FlipBack}>
+                <p>{content}</p>
             </div>
         </div>
+
     );
 }
 
