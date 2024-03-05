4f7d3661d5fe7e99038e1359bfd160207cc7221f
annapandey <149274878+annapandey@users.noreply.github.com>
2024-03-04 22:13:11 -0800

    adding the components from the previous file to this one

--- src/components/getInvolved/card.jsx
+++ src/components/getInvolved/card.jsx
@@ -15,14 +15,15 @@ export async function getStaticProps({ locale }) {
 export default function Card({ cardProps }) {
   const t = useTranslations('GetInvolved.CardComponent');
   return (
-      <div className={styles.cardsContainer}>
-        <div className={styles.card}>
-          <div className={styles.cardImg} style={{backgroundImage: `url(${t(`${cardProps}.imgSrc`)})`}}>
-            <p className={styles.cardText}>{t(`${cardProps}.cardText`)}</p>
-          </div>
-          <button className={styles.detailsButton}>{t('details')}</button>
-          <button className={styles.signUpButton}>{t('signUp')}</button>
+    <div className={styles.cardsContainer}>
+      <div className={styles.card}>
+        <div className={styles.cardImg} style={{ backgroundImage: `url(${t(`${cardProps}.imgSrc`)})` }}>
+          <p className={styles.cardText}>{t(`${cardProps}.cardText`)}</p>
         </div>
+        <button className={styles.detailsButton}>{t('details')}</button>
+        <button className={styles.signUpButton}>{t('signUp')}</button>
       </div>
+    </div>
   );
 }
+
---
+++ src/components/getInvolved/mobilepopupCard.jsx
@@ -0,0 +1,29 @@
+/* add and import styles */
+/* view and test it out by importing it to the respective page */
+import { useTranslations } from 'next-intl';
+import React from 'react';
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import styles from '@/styles/components/getInvolved/mobilepopupCard.module.scss';
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      messages: (await import(`@/messages/${locale}.json`)).default
+    }
+  };
+}
+
+export default function PopupCard({ title, content }) {
+  const t = useTranslations('Popup Card');
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>
+        <p>Close</p><IoIosCloseCircleOutline className={styles.button_svg} />
+      </button>
+      <div className={styles.layout}>
+        <h1>{title}</h1>
+        <p>{content}</p>
+      </div>
+    </div>
+  )
+}
