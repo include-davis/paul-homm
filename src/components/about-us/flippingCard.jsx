71bab08fc11aec3158e70833be3b42b4e1ed112f
Samantha Iraheta <sairaheta1@gmail.com>
2024-03-04 21:51:02 -0800

    fixed rendering & made small design changes

--- src/components/about-us/flippingCard.jsx
+++ src/components/about-us/flippingCard.jsx
@@ -4,39 +4,20 @@ import styles from "@/styles/components/about-us/flippingCard.module.scss";
 
 export default function FlippingCard({ title, content, image, alt }) {
     return (
-
-        <div className={styles.FlipCard}>
+        < div className={styles.container}>
             <h1>{title}</h1>
-            <div className={styles.CardInner}>
-                <div className={styles.CardFront}>
-                    <img src={image} alt={alt} />
+            <div className={styles.FlipCard} >
+                <div className={styles.CardInner}>
+                    <div className={styles.CardFront}>
+                        <img src={image} alt={alt} />
+                    </div>
+                    <div className={styles.FlipBack}>
+                        <p>{content}</p>
+                    </div>
                 </div>
 
-            </div>
-            <div className={styles.FlipBack}>
-                <p>{content}</p>
-            </div>
-        </div>
 
+            </div >
+        </div >
     );
-}
-
-
-/*import { useTranslations } from 'next-intl';
-import React from 'react';
-import styles from "@/styles/components/aboutus/flippingCard.module.scss";
-return (
-    <div className={styles.container}>
-        <h1>{title}</h1>
-        <div className={styles.FlipCard}>
-            <div className={styles.CardInner}></div>
-            <div className={styles.CardFront}>
-                <img src={image} alt={alt} />
-            </div>
-            <div className={styles.FlipBack}>
-                <p>{content}</p>
-            </div>
-        </div>
-    </div>
-);
-*/
+} FlippingCard;
--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -10,12 +10,10 @@ export async function getStaticProps({ locale }) {
     };
 }
 
-export default function About({ }) {
+export default function About() {
     const t = useTranslations('Flipping Cards');
-    const cards = ["Card1", "Card2", "Card3", "Card4", "Card5"];
-    console.log(cards);
-
-
+    // const cards = ["Card1", "Card2", "Card3", "Card4", "Card5"];
+    const cards = ["Card1"];
     const flippingCards = cards.map((card, index) => (
         <FlippingCard
             key={index}
@@ -32,43 +30,4 @@ export default function About({ }) {
         </div>
     );
 }
-/*import React from 'react';
-import FlippingCard from '@/components/about-us/FlippingCard';
-//import FlippingCard from '@/src/components/about-us/FlippingCard';
-export async function getStaticProps({ locale }) {
-    return {
-        props: {
-            messages: (await import(`@/messages/${locale}.json`)).default
-        }
-    };
-}
-export default function About({ messages }) {
-    const t = useTranslations('Flipping Cards');
-    const array = [1, 2, 3, 4, 5];
-    //const flippingCards = array.map((index) => (
-    //<FlippingCard key={index} content={t(`Card${index}.content`)} />
-    //));
-    const flippingCards = array.map((index) => (
-        <FlippingCard key={index} content={t(`Card${index}.content`)} />
-    ));
-    return (
-        <div>
-            {flippingCards}
-        </div>
-    );
-}
-*/
-
-/*export default function FlippingCard({ content, image, title, alt }) {
-    const t = useTranslations('Flipping Cards');
-    const array = [1, 2, 3, 4, 5];
-    return (
-        <>
-            {array.map((index) => (
-                <FlippingCard key={index} content={messages[`Card${index}`].content} />
-            ))}
-        </>
-    );
-}
-*/
 About;
