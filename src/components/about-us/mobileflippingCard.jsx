5452c14b28ae719df553487079406f891bf361af
annapandey <149274878+annapandey@users.noreply.github.com>
2024-03-06 21:11:32 -0800

    styles done, need to work on going to the next card when the button is clicked

--- src/components/about-us/mobileflippingCard.jsx
+++ src/components/about-us/mobileflippingCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useTranslations } from 'next-intl';
 import styles from "@/styles/components/about-us/mobileflippingCard.module.scss";
-import { FaAngleLeft } from "react-icons/fa6";
-import { FaAngleRight } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
+import { FaChevronLeft } from "react-icons/fa";
 
 
 export default function FlippingCard({ title, content, image, alt }) {
@@ -11,8 +11,8 @@ export default function FlippingCard({ title, content, image, alt }) {
             <div className={styles.scroll}>
                 <h1>{title}</h1>
                 <button className={styles.button}>
-                    <FaAngleLeft className={styles.button_svg} />
-                    <FaAngleRight className={styles.button_svg} />
+                    <FaChevronLeft className={styles.button_svg} />
+                    <FaChevronRight className={styles.button_svg} />
                 </button>
             </div>
             <div className={styles.card}>
