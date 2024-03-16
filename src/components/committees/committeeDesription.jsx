3c153d0cc12cb92a060b2efe75c37096d2e732f2
Elysia Oh <102720577+elysiaoh@users.noreply.github.com>
2024-03-15 22:34:10 -0700

    General Styling to Committee descriptions page (#8)
    * General Styling to Committee descriptions page
    
    - Still need to implement back button
    
    * Added button and changed styling
    
    * Updates to description page
    
    * Update en.json
    
    * Update [committee].jsx
    
    * Update committeeDesription.jsx
    
    * translations issue fixed
    
    * changed pathnames to match committees page code
    
    * Styling changes
    
    * final styles changes
    
    ---------
    
    Co-authored-by: JayJ104 <123anjalijain@gmail.com>

--- src/components/committees/committeeDesription.jsx
+++ src/components/committees/committeeDesription.jsx
@@ -1,11 +1,31 @@
+import styles from "@/styles/components/committees/committeeDescription.module.scss"
+import { IoArrowBack } from "react-icons/io5";
+import React from "react";
+import { useTranslations } from "next-intl";
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      messages: (await import(`@/messages/${locale}.json`)).default
+    }
+  };
+}
+
 /* add and import styles */
 /* view and test it out by importing it to the respective page */
 export default function CommitteeDescription({ props }) {
-  const { title, desc } = props;
+  const t = useTranslations(`Committees.CommitteeDescriptionComponent.${props}`);
   return (
-    <div>
-      <h1>{title}</h1>
-      <h2>{desc}</h2>
+    <div className = {styles.wrapper}>
+      <div className = {styles.buttonClass}>
+        <a href="/committees" className = {styles.linkStyle}>
+          <button className = {styles.button}><IoArrowBack /><p>Back to committees</p></button> 
+          </a>
+      </div>
+      <div className = {styles.content}>
+        <h1>{t('title')}</h1>
+        <h2>{t('desc')}</h2>
+      </div>
     </div>
   );
-}
+} 
