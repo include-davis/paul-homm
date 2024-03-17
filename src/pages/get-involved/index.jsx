31627ef9e788278d559e7696f7c24a9d7971f62e
annapandey <149274878+annapandey@users.noreply.github.com>
2024-03-16 21:49:28 -0700

    Feat/popup anna pandey (#11)
    * created files
    
    * Building elements and adding some styles.
    
    My local host won't compile? But it was compiling on Saturday. Regardless this is my updated file that I forgot to push Sunday.
    
    * renamed scss file to fix import issue
    
    * editing styles
    
    * fixed up styles
    
    * The position content seems accurate. I still need to figure out how to move the biegecontainer2 to the right.
    
    * adding the close" image" with tags
    
    * working on the correct format
    
    * updated jsx and styles files
    
    * edits on json
    
    * updated name
    
    * fixed stylings
    
    * fixed rendering and minor styling issues
    
    * adding the components from the previous file to this one
    
    * getting started
    
    * tried to create a toggle component to let one card go to the next. Styles wont render due to an error
    
    * got the functionality to work for the next button. working on styles
    
    * fixed stylings working on button
    
    * working on styles but fixed the buttons
    
    * fixed styling, can't figure out why my header isn't maroon and why my close button is giving me a problem
    
    * almost there
    
    * desktop fix
    
    * consolidated popup card and get involvedstyles
    
    popup card functionality w/o background blur, added a mixin, get involved sm-phone views fixed
    
    * cleaned up repo
    
    ---------
    
    Co-authored-by: JayJ104 <123anjalijain@gmail.com>
    Co-authored-by: Samantha Iraheta <sairaheta1@gmail.com>

--- src/pages/get-involved/index.jsx
+++ src/pages/get-involved/index.jsx
@@ -1,37 +1,38 @@
 import React from "react";
-import styles from "@/styles/pages/get-involved/index.module.scss";
+import styles from "@/styles/pages/get-involved/get-involved.module.scss";
 import { useTranslations } from "next-intl";
-import Card from "@/components/getInvolved/card";
+import Card from "@/components/get-involved/card";
 
 export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      messages: (await import(`@/messages/${locale}.json`)).default,
-    },
-  };
+    return {
+        props: {
+            messages: (await import(`@/messages/${locale}.json`)).default,
+        },
+    };
 }
 
 const cardProps = ["Card1", "Card2", "Card3"];
 export default function GetInvolved() {
-  const t = useTranslations("GetInvolved");
-  
-  return (
-    <div className={styles.sectionContainer}>
-      <h1 className={styles.sectionHeading}>{t('pageTitle')}</h1>
-      <div className={styles.cardContainer}>
-        {cardProps.map((cardProp, index) => (
-          <Card key={index} cardProps={cardProp}/>
-        ))}
-      </div>
-      <div className={styles.donateBoxContainer}>
-        <h3 className={styles.donateHeading}>{t('donateHeading')}</h3>
-        <p className={styles.donateText}>{t('donateText')}</p>
-        <div className={styles.centeredText}>
-          <p className={styles.address}>{t('address')}</p>
-          <p className={styles.donateText}>{t('donateText2')}</p>
+    const t = useTranslations("GetInvolved");
+
+    return (
+        <div className={styles.sectionContainer}>                
+            <h1 className={styles.sectionHeading}>{t('pageTitle')}</h1>
+            <div className={styles.cardContainer}>
+                {cardProps.map((cardProp, index) => (
+                    <Card key={index} cardProps={cardProp} />
+                ))}
+            </div>
+
+            <div className={styles.donateBoxContainer}>
+                <h3 className={styles.donateHeading}>{t('donateHeading')}</h3>
+                <p className={styles.donateText}>{t('donateText')}</p>
+                <div className={styles.centeredText}>
+                    <p className={styles.address}>{t('address')}</p>
+                    <p className={styles.donateText}>{t('donateText2')}</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 GetInvolved;
