63381f3da504fb59425f806bad96eae6bf35bbc5
Samantha Iraheta <sairaheta1@gmail.com>
2024-03-17 02:31:40 -0700

    small fixes to avoid adjusted heights

--- src/components/services/imageSliderMobile.jsx
+++ src/components/services/imageSliderMobile.jsx
@@ -2,7 +2,7 @@ import styles from "@/styles/components/services/imageSliderMobile.module.scss"
 
 import Image from "next/image";
 import { useState } from "react";
-import { MdArrowForwardIos, MdArrowBackIos} from "react-icons/md";
+import { MdArrowForwardIos, MdArrowBackIos } from "react-icons/md";
 import { useTranslations } from 'next-intl';
 import DropDown from "@/components/dropDown/imageSliderDropdown.jsx"
 
@@ -18,7 +18,7 @@ export async function getStaticProps({ locale }) {
 function calcDividor(numFramesShown, primary, scaleDiff) {
     let sum = primary;
     let temp = sum;
-    for(let i = 1; i < numFramesShown; i++) {
+    for (let i = 1; i < numFramesShown; i++) {
         temp -= scaleDiff;
         sum += temp;
     }
@@ -37,28 +37,28 @@ const x = `calc(${contentSpace} / ${dividor})` // x = (W - 2g) / 2.6
 const shiftAmount = `calc(${x} * 0.74 + ${g})` // amount that slider shifts when service changes
 
 function ImageSliderMobileFrame(props) {
-  const { selfIndex, activeIndex, image } = props
-  const dist = Math.abs(selfIndex - activeIndex)
+    const { selfIndex, activeIndex, image } = props
+    const dist = Math.abs(selfIndex - activeIndex)
 
-  function getSizeClass(dist) {
-    switch(dist) {
-      case 0:
-        return styles.primary
-      default:
-        return styles.secondary
+    function getSizeClass(dist) {
+        switch (dist) {
+            case 0:
+                return styles.primary
+            default:
+                return styles.secondary
+        }
     }
-  }
 
-  return (
-      <div className={`${styles.frame} ${getSizeClass(dist)}`}>
-        <Image src={image}
-            style={{ objectFit: "fill" }}
-            fill={true}
-            alt="Insert alt"
-            sizes={"(max-width: 1048px) 100vw"}
-            priority={image === "healthEdu.png" || image === "vaccines.png" ? "true" : "false"} // check for LCP elements
-        />
-      </div>
+    return (
+        <div className={`${styles.frame} ${getSizeClass(dist)}`}>
+            <Image src={image}
+                style={{ objectFit: "fill" }}
+                fill={true}
+                alt="Insert alt"
+                sizes={"(max-width: 1048px) 100vw"}
+                priority={image === "healthEdu.png" || image === "vaccines.png" ? "true" : "false"} // check for LCP elements
+            />
+        </div>
     )
 }
 
@@ -79,21 +79,21 @@ export default function ImageSliderMobile() {
     const imagesFront = images.slice(0, 1)
     const imagesBack = images.slice(-1)
     const processedImages = imagesBack.concat(images).concat(imagesFront)
-    
+
     // Move left and right through images AND change dropdown title
     const changeService = (index) => {
         setActiveIndex(index)
         setService(services[index])
     }
     const subIndex = () => { // Move left
-      const left = (activeIndex + n - 1) % n
-      changeService(left)
+        const left = (activeIndex + n - 1) % n
+        changeService(left)
     }
     const addIndex = () => { // Move right
-      const right = (activeIndex + 1) % n
-      changeService(right)
+        const right = (activeIndex + 1) % n
+        changeService(right)
     }
-    
+
     // Keep track of bullets below images
     const bulletCount = parseInt(t(`${service}.bulletCount`))
     const bullets = [...Array(bulletCount).keys()];;
@@ -101,73 +101,73 @@ export default function ImageSliderMobile() {
     return (
         <div className={styles.main_container}>
             {/* Dropdown */}
-            <div className={service.length < 25 ? styles.dropdown_oneline : styles.dropdown_twolines}>
-                <DropDown services={services} setImage={setActiveIndex} currService={service} setService={setService}/>
+            <div className={styles.dropdown_twolines}>
+                <DropDown services={services} setImage={setActiveIndex} currService={service} setService={setService} />
             </div>
 
             {/* Image Carousel */}
             <div className={styles.window_container}>
                 {/* Button to go left */}
                 <button className={styles.arrow} onClick={subIndex}>
-                    <MdArrowBackIos/>
+                    <MdArrowBackIos />
                 </button>
-          
+
                 {/* Image Slider */}
                 <div className={styles.viewport_container}>
                     <div className={styles.viewport}>
-                        
+
                         {/* Embeds processed images each into ImageSliderMobileFrame */}
-                        <div 
+                        <div
                             className={styles.content_belt}
-                            style={{transform: `translateX(calc(${-(activeIndex)} * ${shiftAmount}))`}}
+                            style={{ transform: `translateX(calc(${-(activeIndex)} * ${shiftAmount}))` }}
                         >
-                            { processedImages.map((frame, index) => {
+                            {processedImages.map((frame, index) => {
                                 return (
-                                    <ImageSliderMobileFrame 
-                                        key={index} 
-                                        selfIndex={index - 1} 
+                                    <ImageSliderMobileFrame
+                                        key={index}
+                                        selfIndex={index - 1}
                                         activeIndex={activeIndex}
                                         image={frame}
                                     />
                                 )
-                            }) 
+                            })
                             }
                         </div>
                     </div>
                 </div>
-          
+
                 {/* Button to go right */}
                 <button className={styles.arrow} onClick={addIndex}>
-                    <MdArrowForwardIos/>
+                    <MdArrowForwardIos />
                 </button>
-           
+
             </div>
-            
+
             {/* Dots below slider */}
             <div className={styles.dots}>
-                { images.map((frame, index) => {
+                {images.map((frame, index) => {
                     return (
-                        <div 
-                        key={index} 
-                        className={`${styles.dot} ${activeIndex === index ? styles.active : null}`}
-                        onClick={() => changeService(index)} // Set dropdown and image index
+                        <div
+                            key={index}
+                            className={`${styles.dot} ${activeIndex === index ? styles.active : null}`}
+                            onClick={() => changeService(index)} // Set dropdown and image index
                         ></div>
                     )
-                    }) 
+                })
                 }
             </div>
-            
+
             {/* Text under corresponding image */}
             <div className={styles.info}>
                 <ul className={styles.info_items}>
-                    { bullets.map((bullet, index) => {
+                    {bullets.map((bullet, index) => {
                         return (
                             <li className={styles.info_item} key={index}>{t(`${service}.bullet${bullet + 1}`)}</li>
                         )
                     })}
                 </ul>
             </div>
-            
+
         </div>
     );
-  }
+}
