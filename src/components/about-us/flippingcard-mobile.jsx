38b15f3c56151159aa1ab1ff9636f7a8b3337865
Samantha Iraheta <sairaheta1@gmail.com>
2024-05-11 23:38:13 -0700

    Squashed commit of the following:
    commit c8da90705c4ea3a377b0a2bd223d29978efc2f8d
    Author: Nandhana Selvam <nandhanaselvam@gmail.com>
    Date:   Thu Mar 14 00:32:58 2024 -0700
    
        fixed card sizing + added border radius
    
    commit 4b255c4cc0b4f8e52414e5113ea2a14bcf7e619b
    Author: Nandhana Selvam <nandhanaselvam@gmail.com>
    Date:   Fri Mar 8 23:59:29 2024 -0800
    
        finished majority of mobile flipping cards
    
    commit acc1e7b063686a2593ae57f0fbe5f254e9ece10d
    Author: Nandhana Selvam <nandhanaselvam@gmail.com>
    Date:   Wed Mar 6 01:54:16 2024 -0800
    
        implemented two carousels for title and cards
    
    commit 1997f538511de22c990cc3e3eb99ee0b9c19cb7a
    Author: Nandhana Selvam <nandhanaselvam@gmail.com>
    Date:   Mon Mar 4 18:51:48 2024 -0800
    
        created mobile flipping cards, started carousel
    
    commit 235fd77cff1eaec5b3356bbdc8e29284d0b3aa3d
    Author: Nandhana Selvam <nandhanaselvam@gmail.com>
    Date:   Sun Mar 3 18:14:09 2024 -0800
    
        made rough flipping card component and rendered them

---
+++ src/components/about-us/flippingcard-mobile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useTranslations } from "next-intl";
+import styles from "@/styles/components/about-us/flippingcard-mobile.module.scss";
+import Image from 'next/image'
+
+export async function getStaticProps({ locale }) {
+    return {
+      props: {
+        messages: (await import(`@/messages/${locale}.json`)).default
+      }
+    };
+}
+
+export default function FlippingCardMobile({props, dimensions}) {
+    const t = useTranslations(`FlippingCards.${props}`)
+    const [height, width] = dimensions;
+    return (
+        <div className={styles.flipping_card}>
+            <p>{t('content')}</p>
+            <div className={styles.image_container}
+                style={{height: height, width: width}}>
+                <Image
+                    src={t('image')}
+                    alt={t('alt')}
+                    style={{ objectFit: "cover" }}
+                    fill={true}
+                />
+            </div>
+        </div>
+    )
+}
--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -13,6 +13,90 @@ export async function getStaticProps({ locale }) {
             messages: (await import(`@/messages/${locale}.json`)).default
         }
     };
+import React from 'react';
+import FlippingCardMobile from "@/components/about-us/flippingcard-mobile"
+import styles from'@/styles/pages/about/about.module.scss'
+import { useState } from 'react';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useTranslations } from "next-intl";
+
+
+export async function getStaticProps({ locale }) {
+    return {
+      props: {
+        messages: (await import(`@/messages/${locale}.json`)).default
+      }
+    };
+}
+
+const data = ["card1", "card2","card3","card4","card5"]
+const dimensions = [
+    ["220px", "168px"],
+    ["226px", "199px"],
+    ["186px", "289px"],
+    ["195px", "291px"],
+    ["212px", "291px"]
+]
+
+export default function About()
+{
+    const [activeIndex, setActiveIndex] = useState(0)
+    const n = data.length
+    const subIndex = () => {
+      setActiveIndex((activeIndex + n - 1) % n)
+    }
+
+    const addIndex = () => {
+      setActiveIndex((activeIndex + 1) % n)
+    }
+
+    const t = useTranslations(`FlippingCards`)
+
+    return(
+        <div className={styles.main_container}>
+            <div className={styles.window_container}>
+                <button className={styles.arrow} onClick={subIndex}>
+                    <IoIosArrowBack/>
+                </button>
+
+                <div className={styles.viewport}> 
+                    <div className={styles.content_belt}
+                    style={{transform: `translateX(calc(${-activeIndex} * 100%))`}}>
+                        <div className={styles.flipping_cards}>
+                            {data.map((card, index) => (
+                                <div key={index} className={styles.frame}> 
+                                    <h1 className={styles.frame_content}>{t(`${card}.title`)}</h1>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                <button className={styles.arrow} onClick={addIndex}>
+                    <IoIosArrowForward />
+                </button>
+            </div>
+
+            <div className={styles.cards_container}>
+                <div className={styles.viewport}> 
+                    <div className={styles.content_belt}
+                    style={{transform: `translateX(calc(${-activeIndex} * 100%))`}}>
+                        <div className={styles.flipping_cards}>
+                            {data.map((card, index) => (
+                                <div key={index} className={styles.frame}> 
+                                    <FlippingCardMobile props = {card} dimensions =
+                                    {dimensions[index]} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+            
+        </div>
+        
+    )
 }
 
 /* TODO
