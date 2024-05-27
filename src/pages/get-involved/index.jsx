e87508b96ddd2747451666c66ac524ca9f650972
Jay <116294517+JayJ104@users.noreply.github.com>
2024-05-26 22:55:58 -0700

    Fix/deployment (#35)
    * Revert "gallery div warning fixed"
    
    This reverts commit 264747260d24268edb3f4a95833c82709553c8b3.
    
    * Revert "getStaticProps added everywhere, added logo asset"
    
    This reverts commit 497e286786bb518cc1cd25b91846845515ea0ea6.
    
    * Revert "footer image issue resolved"
    
    This reverts commit eedaf1375cb6844e4c6d6449c019c4dc28bb2dc9.
    
    * Revert "Origin/feat/mobile aboutus elysia (#26)"
    
    This reverts commit 043f6ff592aa9df17b1da1b0d02d998c0128cd1a.
    
    * Revert "get involved images recovered"
    
    This reverts commit e88f69a6edd180518189531961c9a39003e5c18a.
    
    * Revert "Merge pull request #32 from include-davis/feat/services-page-mobile-cristian"
    
    This reverts commit 1848a085797a05ab25f03ef0c1458f2df1ab482c, reversing
    changes made to 0212ed9ae553e81b1644450a82f9d3866c001712.
    
    * Update .eslintrc.json
    
    * added 404 and messages to all locales
    
    also homepage gallery fix
    
    * linting fixes
    
    * removed header-footer from app
    
    * FIXED FINALLY (+metadata)
    
    * recommited sam's merge
    
    * merge conflict fixes

--- src/pages/get-involved/index.jsx
+++ src/pages/get-involved/index.jsx
@@ -2,38 +2,41 @@ import React from "react";
 import styles from "@/styles/pages/get-involved/get-involved.module.scss";
 import { useTranslations } from "next-intl";
 import Card from "@/components/get-involved/card";
+import PageLayout from "@/components/layout";
 
 export async function getStaticProps({ locale }) {
-    return {
-        props: {
-            messages: (await import(`@/messages/${locale}.json`)).default,
-        },
-    };
+  return {
+    props: {
+      messages: (await import(`@/messages/${locale}.json`)).default,
+    },
+  };
 }
 
 const cardProps = ["Card1", "Card2", "Card3"];
 export default function GetInvolved() {
-    const t = useTranslations("GetInvolved");
+  const t = useTranslations("GetInvolved");
 
-    return (
-        <div className={styles.sectionContainer}>                
-            <h1 className={styles.sectionHeading}>{t('pageTitle')}</h1>
-            <div className={styles.cardContainer}>
-                {cardProps.map((cardProp, index) => (
-                    <Card key={index} cardProps={cardProp} />
-                ))}
-            </div>
-
-            <div className={styles.donateBoxContainer}>
-                <h3 className={styles.donateHeading}>{t('donateHeading')}</h3>
-                <p className={styles.donateText}>{t('donateText')}</p>
-                <div className={styles.centeredText}>
-                    <p className={styles.address}>{t('address')}</p>
-                    <p className={styles.donateText}>{t('donateText2')}</p>
-                </div>
-            </div>
+  return (
+    <PageLayout>
+      <div className={styles.sectionContainer}>
+        <h1 className={styles.sectionHeading}>{t("pageTitle")}</h1>
+        <div className={styles.cardContainer}>
+          {cardProps.map((cardProp, index) => (
+            <Card key={index} cardProps={cardProp} />
+          ))}
         </div>
-    );
+
+        <div className={styles.donateBoxContainer}>
+          <h3 className={styles.donateHeading}>{t("donateHeading")}</h3>
+          <p className={styles.donateText}>{t("donateText")}</p>
+          <div className={styles.centeredText}>
+            <p className={styles.address}>{t("address")}</p>
+            <p className={styles.donateText}>{t("donateText2")}</p>
+          </div>
+        </div>
+      </div>
+    </PageLayout>
+  );
 }
 
 GetInvolved;
