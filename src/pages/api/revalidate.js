3cb21e949adeb29cbfdea94aa279a6ae96f4d80f
JayJ104 <123anjalijain@gmail.com>
2024-05-28 22:39:46 -0700

    added revalidation

---
+++ src/pages/api/revalidate.js
@@ -0,0 +1,15 @@
+export default async function handler(_, res){
+    try{
+        await res.revalidate("/");
+        await res.revalidate("/about-us");
+        await res.revalidate("/committees");
+        await res.revalidate("/get-involved");
+        await res.revalidate("/services");
+    } catch(e){ 
+        res.send({
+            status: e.status,
+            body: null,
+            error: e.message
+        })
+    }
+}
