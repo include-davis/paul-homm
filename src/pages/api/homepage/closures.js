273ab2ffb20e67bbbd189f7f034ff9cb1c713728
JayJ104 <123anjalijain@gmail.com>
2024-05-28 18:58:04 -0700

    fetching closures

---
+++ src/pages/api/homepage/closures.js
@@ -0,0 +1,47 @@
+export default async function getClosures(req, res){
+    if(req.method === 'GET'){    
+        try{
+            // future dates only
+            const today = new Date().toISOString().split('T')[0];
+            const body = await fetch(`${process.env.CMS_BASE_URL}/api/closure-dates?populate=*&publicationState=live&filters[$and][0][closure_date][$gte]=${today}`, 
+            {
+                method: 'GET',
+                headers: {
+                    Authorization:
+                    `Bearer ${process.env.CMS_API_KEY}`,
+            }})
+
+            const resssss = await body.json();
+
+            if(resssss.data){
+                const dates = resssss.data;
+                res.send({
+                    status: 200,
+                    data: dates.map((dateDoc)=>{
+                        const dateAttributes = dateDoc.attributes;
+                        return {
+                            date: dateAttributes.closure_date,
+                        }
+                    })
+                })
+            } else{
+                res.send({
+                    status: 500,
+                    error: 'Could not retrieve data from CMS'
+                })
+            }
+        }catch(e){
+            console.log(e);
+            res.send({
+                status: 500,
+                error: 'Internal server error'
+            })
+        }
+    } else {
+        res.send({
+            status: 400,
+            error: 'Bad request'
+        })
+    }
+}
+  
