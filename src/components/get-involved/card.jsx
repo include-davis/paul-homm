34f31d2cb03a753e07d68a2c0f992abaf680b4c2
JayJ104 <123anjalijain@gmail.com>
2024-06-05 14:02:59 -0700

    added sign up links to get-involved

--- src/components/get-involved/card.jsx
+++ src/components/get-involved/card.jsx
@@ -1,9 +1,10 @@
 /* add and import styles */
 /* view and test it out by importing it to the respective page */
-import styles from "@/styles/components/get-involved/cards.module.scss";
 import { useTranslations } from "next-intl";
-import PopupCard from "./popupCard";
 import { useState } from "react";
+import Link from "next/link";
+import styles from "@/styles/components/get-involved/cards.module.scss";
+import PopupCard from "./popupCard";
 
 export default function Card({ cardProps }) {
   const { card, imgSrc } = cardProps;
@@ -26,7 +27,13 @@ export default function Card({ cardProps }) {
         <button className={styles.detailsButton} onClick={togglePopup}>
           {t("details_text")}
         </button>
-        <button className={styles.signUpButton}>{t("sign_up_text")}</button>
+        <Link
+          href={t(`${card}.sign_up_link`)}
+          target="_blank"
+          className={styles.links}
+        >
+          <button className={styles.signUpButton}>{t("sign_up_text")}</button>
+        </Link>
       </div>
       {popup && <PopupCard card={card} onClose={togglePopup} />}
     </div>
